15fb44c7f5234bc8324c1fc56594b33b32ab4e7d
sdyer777 <sdyer777@yahoo.com>
2017-09-03 17:48:20 -0400

    Made public directory, with HTML

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const app = express()
-app.get('/', (req, res) => {
-  res.send('HEY! This has been CHANGED again, once more!!')
-})
+//app.get('/', (req, res) => {
+//  res.send('HEY! This has been CHANGED again, once more!!')
+//})
+app.use(express.static('public'))
 app.listen(3000, () => console.log('Server running on port 3000'))
